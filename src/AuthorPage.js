c6e09fd21f1f9f1abb0007d9e8b819d1b361d869
Anngineer <65789253+Anngineer@users.noreply.github.com>
2022-10-22 20:28:17 -0400

    Corrects name of banner title

--- src/AuthorPage.js
+++ src/AuthorPage.js
@@ -1,7 +1,7 @@
 import Banner from "./Banner";
-import retrogirl2a from "./images/retrogirl2a.jpg";
+// import retrogirl2a from "./images/retrogirl2a.jpg";
 import authorphoto9 from "./images/authorphoto9.jpg";
-import authorphoto10 from "./images/authorphoto10.jpg";
+// import authorphoto10 from "./images/authorphoto10.jpg";
 
 const AuthorPage = ({ theme, setTheme }) => {
   return (
