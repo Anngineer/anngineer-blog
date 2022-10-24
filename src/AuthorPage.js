f822f4bbe8dc9465819dc24207277bb2644eafb9
Anngineer <65789253+Anngineer@users.noreply.github.com>
2022-10-23 21:56:32 -0400

    Adds author bio and fixes author-body css height error

--- src/AuthorPage.js
+++ src/AuthorPage.js
@@ -8,7 +8,7 @@ const AuthorPage = ({ theme, setTheme }) => {
     <div className="author-page">
       <Banner theme={theme} setTheme={setTheme} />
       <div className="author-body">
-        <h1>Author Information</h1>
+        <h1>Ann Bridge</h1>
         <div className="author-container">
           <div className="image-container">
             <img
@@ -17,13 +17,24 @@ const AuthorPage = ({ theme, setTheme }) => {
             />
           </div>
           <div className="author-description">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
+            Ann is a web developer. With a background in research, she is a
+            published co-author in&nbsp;
+            <a href="https://pubmed.ncbi.nlm.nih.gov/32338550/">
+              Disability and Rehabilitation: Assistive Technology.
+            </a>
+            <br />
+            <br />
+            Having found a passion for teaching engineering topics while at
+            university, she continues to do so by blogging, creating educational
+            YouTube content, and writing story-based educational games. Feel
+            free to peruse her projects on{" "}
+            <a href="https://github.com/Anngineer">GitHub</a> as well.
+            <br />
+            <br />
+            This blog is written to help others discover and explore their love
+            of technology and mathematics. And most importantly, it aims to
+            support readers as they navigate new career challenges and personal
+            growth.
           </div>
         </div>
       </div>
